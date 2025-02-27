dfe6a71b8f59b4bd87053f216b4e401fd0e9ddf9
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2025-02-27 19:48:47 +0330

    first commit

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"], // Entry point
+  format: ["esm", "cjs"], // Output formats
+  dts: true, // Generate TypeScript declaration files
+  clean: true, // Clean the output directory before building
+  sourcemap: true, // Generate sourcemaps
+});
