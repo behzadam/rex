50c3e6263d37eebccb665e94d5c4bac68382c4fa
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2025-03-03 21:46:19 +0330

    test: add min | max tests

--- src/__tests__/string.test.ts
+++ src/__tests__/string.test.ts
@@ -1,5 +1,16 @@
 import { rex } from '../index'
-test('string infer', () => {
-  const s = rex.string()
-  type sType = rex.infer<typeof s>
+
+const minFive = rex.string().min(5, 'min5')
+const maxFive = rex.string().max(5, 'max5')
+
+test('passing validations', () => {
+  minFive.parse('12345')
+  minFive.parse('123456')
+  maxFive.parse('12345')
+  maxFive.parse('1234')
+})
+
+test('failing validations', () => {
+  expect(() => minFive.parse('1234')).toThrow()
+  expect(() => maxFive.parse('123456')).toThrow()
 })
